fc31781c30100c37598147828daffc38eb4c1d9b
Connor Watts <connorwatts@hotmail.com>
2022-11-08 14:29:17 +0000

    download stock list

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+from utils import download_stock_list
 
-def main():
+
+def main(params):
+
+    # if not all ready download list of stocks from wiki page for SP500
+    if params["download_stock_list"]: download_stock_list(params)
+
+
+    #get and save data as selenium
+    #create stock classes (maybe getting the data is a method in class)
+
     pass
 
 
 if __name__ == '__main__':
-    main()
+    params = {"download_stock_list":False}
+    main(params)
