fb9b0c7b02216da81807ff367f648c0e25d1a0bb
Connor Watts <connorwatts@hotmail.com>
2022-12-08 10:50:30 +0000

    app

--- pair.py
+++ pair.py
@@ -1,4 +1,12 @@
+from statsmodels.tsa.stattools import coint
+
+
 class Pair():
-    def __init__(self, ticker1, ticker2):
-        self.ticker1 = ticker1
-        self.ticker2 = ticker2
+    def __init__(self, stock1, stock2):
+        self.stock1 = stock1
+        self.ticker1 = stock1.ticker
+        self.stock2 = stock2
+        self.ticker2 = stock2.ticker
+
+    def calc_engle_granger(self):
+        self.coint = coint(self.stock1.close,self.stock2.close)
